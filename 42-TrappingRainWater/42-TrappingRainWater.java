8bb630c5f26885d19ebbdd006db0cb07929c3f50
Abhishek singh <abhisheksingha96101@gmail.com>
2026-07-21 17:02:14 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -0,0 +1,28 @@
+// Last updated: 7/21/2026, 5:02:13 PM
+class Solution {
+    public int trap(int[] height) {
+        int sum = 0;
+
+        int left[] = new int[height.length];
+        int right[] = new int[height.length];
+
+        // Left max array
+        left[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            left[i] = Math.max(left[i - 1], height[i]);
+        }
+
+        // Right max array
+        right[height.length - 1] = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i--) {
+            right[i] = Math.max(height[i], right[i + 1]);
+        }
+
+        // Calculate trapped water
+        for (int i = 0; i < height.length; i++) {
+            sum += Math.min(left[i], right[i]) - height[i];
+        }
+
+        return sum;
+    }
+}
