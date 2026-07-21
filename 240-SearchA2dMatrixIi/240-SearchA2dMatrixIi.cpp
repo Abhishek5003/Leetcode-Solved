e7a17e3c828df2e6453bb7f7076d49a23083feff
Abhishek singh <abhisheksingha96101@gmail.com>
2026-07-21 17:01:16 +0530

    Create 240-SearchA2dMatrixIi

---
+++ 240-SearchA2dMatrixIi/240-SearchA2dMatrixIi.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/21/2026, 5:01:15 PM
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        bool ispresent = false;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {     
+                if(matrix[i][j] == target){
+                    ispresent = true;
+                    return ispresent;
+                }
+            }
+        }
+        return ispresent;
+    }
+};
