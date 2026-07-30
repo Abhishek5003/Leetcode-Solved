4becccedae227c5772653a504819f6a2a5599265
Abhishek singh <abhisheksingha96101@gmail.com>
2026-07-30 07:00:00 +0530

    Create 448-FindAllNumbersDisappearedInAnArray

---
+++ 448-FindAllNumbersDisappearedInAnArray/448-FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,19 @@
+// Last updated: 7/30/2026, 7:00:00 AM
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        boolean[] present = new boolean[nums.length + 1];
+        List<Integer> ans = new ArrayList<>();
+
+        for (int num : nums) {
+            present[num] = true;
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (!present[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
