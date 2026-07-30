e1e4636c1b7f51a884fe282a3e974178c1fd2165
Abhishek singh <abhisheksingha96101@gmail.com>
2026-07-30 06:59:27 +0530

    Create 2571-FindThePivotInteger

---
+++ 2571-FindThePivotInteger/2571-FindThePivotInteger.java
@@ -0,0 +1,17 @@
+// Last updated: 7/30/2026, 6:59:25 AM
+class Solution {
+    public int pivotInteger(int n) {
+        int total = n * (n + 1) / 2;
+        int leftSum = 0;
+
+        for (int x = 1; x <= n; x++) {
+            leftSum += x;
+            int rightSum = total - leftSum + x;
+
+            if (leftSum == rightSum) return x;
+            
+        }
+
+        return -1; 
+    }
+}
